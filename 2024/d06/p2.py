8d4abcbe034e667448c320fd21dc2085bdf02ca7
Kraktoos <kraktoos@gmail.com>
2024-12-06 19:37:52 +0000

    Finally finish day 6 p2

--- 2024/d06/p2.py
+++ 2024/d06/p2.py
@@ -1,57 +1,88 @@
-import copy
+from copy import deepcopy
 
 matrix = [[x for x in line] for line in open("input.txt", "r").read().strip().splitlines()]
-
-def get_right_turn(going):
-  if going[0] == -1:
-    return (0, 1)
-  elif going[0] == 1:
-    return (0, -1)
-  elif going[1] == -1:
-    return (-1, 0)
-  elif going[1] == 1:
-    return (1, 0)
-
-n_times_until_probably_loop = len(matrix) ** 3
-
-def will_finish(curr_matrix, starting_pos, starting_going):
-  curr_matrix = copy.deepcopy(curr_matrix)
-  going = starting_going
-
-  count_times = 0
-  while True:
-    curr_matrix[starting_pos[0]][starting_pos[1]] = "X"
-    count_times += 1
-
-    if starting_pos[0] + going[0] <= -1 or starting_pos[0] + going[0] >= len(curr_matrix[0]) or starting_pos[1] + going[1] <= -1 or starting_pos[1] + going[1] >= len(curr_matrix):
-      break
-
-    if curr_matrix[starting_pos[0] + going[0]][starting_pos[1] + going[1]] == "#":
-      going = get_right_turn(going)
-
-    starting_pos = (starting_pos[0] + going[0], starting_pos[1] + going[1])
-
-    if count_times >= n_times_until_probably_loop:
-      return False
-
-  return True
+original_matrix = deepcopy(matrix)
 
 current_pos = (0, 0)
 for i in range(len(matrix)):
   for j in range(len(matrix[0])):
     if matrix[i][j] == "^":
+      matrix[i][j] = "u"
       current_pos = (i, j)
       break
 
 going = (-1, 0)
+
+while True:
+  match going:
+    case (-1, 0):
+      matrix[current_pos[0]][current_pos[1]] = "u"
+    case (1, 0):
+      matrix[current_pos[0]][current_pos[1]] = "d"
+    case (0, -1):
+      matrix[current_pos[0]][current_pos[1]] = "l"
+    case (0, 1):
+      matrix[current_pos[0]][current_pos[1]] = "r"
+
+  if current_pos[0] + going[0] <= -1 or current_pos[0] + going[0] >= len(matrix[0]) or current_pos[1] + going[1] <= -1 or current_pos[1] + going[1] >= len(matrix):
+    break
+
+  if matrix[current_pos[0] + going[0]][current_pos[1] + going[1]] == "#":
+    if going[0] == -1:
+      going = (0, 1)
+    elif going[0] == 1:
+      going = (0, -1)
+    elif going[1] == -1:
+      going = (-1, 0)
+    elif going[1] == 1:
+      going = (1, 0)
+  
+
+  current_pos = (current_pos[0] + going[0], current_pos[1] + going[1])
+
+def can_solve(new_matrix, cpos, cgoing):
+  visited = set()
+
+  while True:
+    next_row, next_col = cpos[0] + cgoing[0], cpos[1] + cgoing[1]
+
+    if next_row < 0 or next_row >= len(matrix) or next_col < 0 or next_col >= len(matrix[0]):
+      return True
+    
+    state = (cpos, cgoing)
+    if state in visited:
+      return False
+    visited.add(state)
+
+    if new_matrix[next_row][next_col] == "#":
+      if cgoing[0] == -1:
+        cgoing = (0, 1)
+      elif cgoing[0] == 1:
+        cgoing = (0, -1)
+      elif cgoing[1] == -1:
+        cgoing = (-1, 0)
+      elif cgoing[1] == 1:
+        cgoing = (1, 0)
+    else:
+      cpos = (next_row, next_col)
+
+current_pos = (0, 0)
+for i in range(len(original_matrix)):
+  for j in range(len(original_matrix[0])):
+    if original_matrix[i][j] == "^":
+      original_matrix[i][j] = "u"
+      current_pos = (i, j)
+      break
+going = (-1, 0)
+solved_matrix = deepcopy(matrix)
+
 total = 0
-for i in range(len(matrix)):
-  for j in range(len(matrix[0])):
-    if matrix[i][j] == ".":
-      matrix[i][j] = "#"
-      if not will_finish(matrix, current_pos, going):
+for i in range(len(original_matrix)):
+  for j in range(len(original_matrix[0])):
+    if solved_matrix[i][j] in ["u", "d", "l", "r"] and original_matrix[i][j] == ".":
+      original_matrix[i][j] = "#"
+      if not can_solve(original_matrix, current_pos, going):
         total += 1
-      matrix[i][j] = "."
-  print(i)
+      original_matrix[i][j] = "."
 
 print(total)
--- README.md
+++ README.md
@@ -14,6 +14,7 @@ This repository contains my solutions to the problems I've solved from Advent of
 | 03 | [Mull It Over](https://adventofcode.com/2024/day/3) | ⭐⭐ |
 | 04 | [Ceres Search](https://adventofcode.com/2024/day/4) | ⭐⭐ |
 | 05 | [Print Queue](https://adventofcode.com/2024/day/5) | ⭐⭐ |
+| 06 | [Guard Gallivant](https://adventofcode.com/2024/day/6) | ⭐⭐ |
 
 ### 2023
 | Day | Name | Stars |
