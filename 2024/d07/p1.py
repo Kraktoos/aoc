469f11397742a604b0241de7fb6cd38bb80f2c5f
Kraktoos <kraktoos@gmail.com>
2024-12-07 10:38:07 +0000

    Day 7

---
+++ 2024/d07/p1.py
@@ -0,0 +1,17 @@
+lines = [x.split(": ") for x in open("input.txt", "r").read().strip().splitlines()]
+data = [(int(line[0]), list(map(int, line[1].split(" ")))) for line in lines]
+
+total = 0
+for excepted_result, numbers in data:
+  current_results = {numbers[0]}
+  for number in numbers[1:]:
+    old_results = current_results.copy()
+    current_results.clear()
+    for curr in old_results:
+      current_results.add(curr + number)
+      current_results.add(curr * number)
+  for actual_result in current_results:
+    if actual_result == excepted_result:
+      total += actual_result
+      continue
+print(total)
