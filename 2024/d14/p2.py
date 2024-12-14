a82b08d90d69c310c5e0a79d8d426ae0c7986a25
Kraktoos <kraktoos@gmail.com>
2024-12-14 12:56:06 +0000

    p2 d14 sucks man

--- 2024/d14/p2.py
+++ 2024/d14/p2.py
@@ -1,13 +1,14 @@
-# I still don't get part 2
-
 import re
+from math import prod
 
 robots = open("input.txt", "r").read().splitlines()
 
 w, h = 101, 103
-seconds = 5000
+seconds = w * h
 
-for sec_att in range(seconds):
+curr_min = (float("inf"), float("inf"))
+
+for sec_att in range(1, seconds):
   matrix = [[0 for _ in range(w)] for __ in range(h)]
   for robot in robots:
     px, py, vx, vy = map(int, re.findall(r"([+-]?\d+)", robot))
@@ -15,21 +16,15 @@ for sec_att in range(seconds):
     py = ((py+1) + vy * sec_att) % h - 1
     matrix[py][px] += 1
 
+  break_all = False
+  for l in matrix:
+    for item in l:
+      if item not in {0, 1}:
+        break_all = True
+        break
+    if break_all: break
 
-  top_left =  [matrix[i][:w // 2] for i in range(h // 2)]
-  top_right = [matrix[i][w // 2+1:] for i in range(h // 2)]
-  bot_left =  [matrix[i][:w // 2] for i in range(h // 2+1, h)]
-  bot_right = [matrix[i][w // 2+1:] for i in range(h // 2+1, h)]
-  sum_top_left = sum([sum(x) for x in top_left])
-  sum_top_right = sum([sum(x) for x in top_right])
-  sum_bot_left = sum([sum(x) for x in bot_left])
-  sum_bot_right = sum([sum(x) for x in bot_right])
+  if break_all: continue
 
-  if sum_bot_left == sum_bot_right and sum_top_left == sum_top_right and sum_bot_left > sum_top_left:
-    for l in matrix:
-      print(("".join([str(x) for x in l])).replace("0", "."))
-    
-    print("")
-
-
-  # print(prod([sum([sum(x) for x in top_left]), sum([sum(x) for x in top_right]), sum([sum(x) for x in bot_left]), sum([sum(x) for x in bot_right])]))
+  print(sec_att)
+  break
